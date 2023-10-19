b822a93a6257854cd2e03008e0407ee241050730
RJLenny <robertlennon@roberts-mbp.myfiosgateway.com>
2023-10-18 22:04:35 -0400

    added files with initial code

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+function generatePassword() {
+  return lowercaseLetters.length;
+};
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
