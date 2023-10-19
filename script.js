b35a2b1e5cde74fbc99050b2d6a5f46ed1565f04
RJLenny <robertlennon@roberts-mbp.myfiosgateway.com>
2023-10-18 23:03:00 -0400

    added input prompt for password length

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var passwordLength = 8;
-var testLetters = ["a","b","c","d","e"];
-
-var randomPassword = "";
-
 function generatePassword() {
+  //Establish input variables to generate password
+  var passwordLength=prompt("Provide a password length:");
+  var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var testLetters = ["a","b","c","d","e"];
+  var randomPassword = "";
+
   for (i=0;i<(passwordLength);i++) {
     //Generate random number from 0-25 corresponding to a letter.
     var randomLetter = Math.floor(Math.random() * (lowercaseLetters.length));
