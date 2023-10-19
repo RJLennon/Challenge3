1d7fcae5572dc6cb27fc02bb3b3326b6577672fc
RJLenny <robertlennon@roberts-mbp.myfiosgateway.com>
2023-10-18 22:30:19 -0400

    password with random lowercase letters

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var passwordLength = 8;
+var testLetters = ["a","b","c","d","e"];
+
+var randomPassword = "";
 
 function generatePassword() {
-  return lowercaseLetters.length;
+  for (i=0;i<(passwordLength);i++) {
+    //Generate random number from 0-25 corresponding to a letter.
+    var randomLetter = Math.floor(Math.random() * (lowercaseLetters.length));
+    //add random letter from array for each iteration of the loop
+    randomPassword += lowercaseLetters[randomLetter];
+  }
+
+  return randomPassword;
 };
 
 function writePassword() {
