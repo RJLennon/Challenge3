033c4819a26d17b4bec74bb875819f0780930fd4
RJLenny <robertlennon@roberts-mbp.myfiosgateway.com>
2023-10-19 01:37:51 -0400

    added password criteria inputs and validation logic

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function generatePassword() {
-  //Establish input variables to generate the password.
-  var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","~","`","-","_","+","=","{","[","}","]","|",":",";","<",",",">",".","?","/"];
-  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-  var randomPassword = "";
 
   //Input password length. Validate it is a number between 8 and 120.
   for (i=0;i<1;) {
@@ -19,23 +13,62 @@ function generatePassword() {
     } else if (passwordLength === null) {
       i=1;
     } else {
-      alert("Invalid Response. Password length must be a number between 8 and 120.");
+      alert("Error: Password length must be a number between 8 and 120.");
     };
   };
 
-  //Input preferences. Validate & Confirm.
+  //Input character preferences. Validate & Confirm.
   for (i=0;i<1;) {
     if (passwordLength === null) {
       i=1;
     } else {
-      var includeLowercase = confirm("Include lowercase letters in password?")
-      var includeUppercase = confirm("Include uppercase letters in password?")
-      var includeNumbers = confirm("Include numbers in password?")
-      var includeSpecialChar = confirm("Include special characters in password?")
-      i=1;
-    }
+      var includeLowercase = confirm("Include lowercase letters in password?");
+      var includeUppercase = confirm("Include uppercase letters in password?");
+      var includeNumbers = confirm("Include numbers in password?");
+      var includeSpecialChar = confirm("Include special characters in password?");
+      
+      //Confirm at least one of the criteria is true.
+      if (includeLowercase||includeUppercase||includeNumbers||includeSpecialChar) {
+        var validateLowercase = "";
+        var validateUppercase = "";
+        var validateNumbers = "";
+        var validateSpecialChar = "";
+
+        //Setup confirmation message.
+        if (includeLowercase) {
+          validateLowercase = "\nLowercase Letters"
+        };
+        if (includeUppercase) {
+          validateUppercase = "\nUppercase Letters"
+        };
+        if (includeNumbers) {
+          validateNumbers = "\nNumbers"
+        };
+        if (includeSpecialChar) {
+          validateSpecialChar = "\nSpecial Characters"
+        };
+        var validatePreferences = confirm("A password will be generated with "+passwordLength+" random characters that will include at least one of the following:\n"+validateLowercase+validateUppercase+validateNumbers+validateSpecialChar+"\n\nCONFIRM?");
+        
+        //Continue if confirmed. End if cancelled.
+        if (validatePreferences) {
+          i=1;
+        } else {
+          passwordLength = null;
+          i=1;
+        }
+      } else {
+        alert ("Error: Must confirm at least one of the provided criteria.")
+      };
+      
+    };
   }
 
+  //Establish input variables to generate the password.
+  var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","~","`","-","_","+","=","{","[","}","]","|",":",";","<",",",">",".","?","/"];
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  var randomPassword = "";
 
   //Generate password of inputted length
   for (i=0;i<(passwordLength);i++) {
